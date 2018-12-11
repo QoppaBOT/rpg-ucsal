6277c1b41334e8e08a3fa776d541e23fe92cb0c6
Luiz Borges <luiiz.bgs@gmail.com>
2018-12-11 17:41:13 -0300

    Removendo warn

--- src/main/java/br/ucsal/sala/controller/ListarSalaInnerServlet.java
+++ src/main/java/br/ucsal/sala/controller/ListarSalaInnerServlet.java
@@ -1,17 +1,13 @@
 package br.ucsal.sala.controller;
 
 import java.io.IOException;
-import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.ucsal.rpg.dao.SalaDAO;
-import br.ucsal.rpg.model.Sala;
 
 /**
 
--- src/main/webapp/AdicionarDangeonForm.jsp
+++ src/main/webapp/AdicionarDangeonForm.jsp
@@ -1,6 +1,6 @@
-<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page language="java" contentType="text/html; charset=utf-8"
 	pageEncoding="ISO-8859-1"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
